bd0ca668ec98deb0049bc38e85c0f16140426f1a
Matheus Stabile <matheus.stabile@gmail.com>
2021-02-05 19:21:14 -0300

    Projeto finalizado

---
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -0,0 +1,46 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");
+
+		String nomeEmpresa = request.getParameter("nome");
+		String paramDataEmpresa = request.getParameter("data");
+
+		Date dataAbertura = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+			dataAbertura = sdf.parse(paramDataEmpresa);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+
+		Empresa empresa = new Empresa();
+		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
+
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+
+		request.setAttribute("empresa", empresa.getNome());
+
+		response.sendRedirect("listaEmpresas");
+	}
+
+}
